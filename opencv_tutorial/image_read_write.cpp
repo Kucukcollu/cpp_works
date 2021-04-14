2e565248293011cb9ebfb71f224df7ea06e31a40
Kucukcollu <kucukcollu@outlook.com>
2021-04-14 17:48:34 +0300

    comments added

--- opencv_tutorial/image_read_write.cpp
+++ opencv_tutorial/image_read_write.cpp
@@ -5,21 +5,16 @@ void ImageReadWrite()
 {
     std::cout << "Image read write" << std::endl;
     
-    //ClearImages();
-    
+    // image path
     cv::Mat  image=cv::imread("/home/ronux/cpp_works/opencv_tutorial/f16.jpg");
 
     if(image.data)
     {
         cv::namedWindow("Fly");
         cv::imshow("Fly",image);
-
         std::cout << "Fly is loaded press any key ..." << std::endl;
-
         cv::waitKey(0);
-
         cv::imwrite("/home/ronux/cpp_works/opencv_tutorial/f16.png",image);
-
         cv::destroyAllWindows();
 
     }
@@ -33,6 +28,5 @@ void ImageReadWrite()
 int main()
 {
     ImageReadWrite();
-
     return 0;
 }
